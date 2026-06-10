85c2fd977b3aad6f5d9da6d3a00d598f1f17c25b
zju1229 <3240101229@zju.edu.cn>
2026-06-11 01:28:01 +0800

    Delete utils directory

--- utils/image_saver.py
+++
@@ -1,8 +0,0 @@
-import os
-import matplotlib.pyplot as plt
-
-def save_figure(filename, dpi=100):
-    """保存当前 matplotlib 图形到 static 目录"""
-    os.makedirs("static", exist_ok=True)
-    plt.savefig(os.path.join("static", filename), dpi=dpi, bbox_inches='tight')
-    plt.close()
